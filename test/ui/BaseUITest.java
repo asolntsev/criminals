94f73dd689495bab3052b79c8a7aeb6179072f1a
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-09-01 23:18:52 +0300

    try full text search (available from Selenide 6.7.0)
    see https://selenide.org/2022/08/04/selenide-6.7.0/

--- test/ui/BaseUITest.java
+++ test/ui/BaseUITest.java
@@ -5,6 +5,8 @@ import criminals.Application;
 import org.junit.Before;
 import play.Play;
 
+import static com.codeborne.selenide.TextCheck.FULL_TEXT;
+
 
 public class BaseUITest {
   @Before
@@ -15,6 +17,7 @@ public class BaseUITest {
       Configuration.baseUrl = "http://0.0.0.0:9000";
       Configuration.browserSize = "1024x800";
       Configuration.browser = "chrome";
+      Configuration.textCheck = FULL_TEXT;
     }
   }
 }
