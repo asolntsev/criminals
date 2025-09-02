ed4e6b849e99449338d174708e1ef4a503091a3a
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-09-02 10:56:07 +0300

    upgrade Replay from 2.3.1 to 2.7.1

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -4,9 +4,12 @@ import play.mvc.Controller;
 import play.mvc.results.Result;
 import play.rebel.View;
 
+import static java.util.Objects.requireNonNullElse;
+import static java.util.Objects.requireNonNullElseGet;
+
 public class Dashboard extends Controller {
 
-  public Result index() {
-    return new View("dashboard/index.html").with("name", session.get("user.name"));
+  public Result index(String q) {
+    return new View("dashboard/index.html").with("name", requireNonNullElseGet(q, () -> requireNonNullElse(session.get("user.name"), "")));
   }
 }
